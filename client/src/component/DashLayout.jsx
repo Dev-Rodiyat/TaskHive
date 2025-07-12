72bd05e7a23822f0703e979b4f43985c68d93ee7
Dev-Rodiyat <olajideadedoyinrodiyat@gmail.com>
2025-07-12 22:31:30 +0100

    deploying

--- client/src/component/DashLayout.jsx
+++ client/src/component/DashLayout.jsx
@@ -11,7 +11,6 @@ import DeleteAccount from './modals/DeleteAccount';
 import Logout from './modals/Logout';
 import AddTaskModal from './task/AddTaskModal';
 
-
 export default function DashLayout() {
     const [activeTab, setActiveTab] = useState('dashboard');
     const [notificationModalOpen, setNotificationModalOpen] = useState(false);
--- client/src/component/Dashboard.jsx
+++ client/src/component/Dashboard.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 import taskImage from './../assets/task.png';
 import { motion } from 'framer-motion';
-import AddTaskModal from './Modals/AddTaskModal';
 import WeeklyProgressChart from './WeeklyProgressChart';
 import { useUser } from './context/UserContext';
 import { ClipLoader } from 'react-spinners';
 import { useTaskContext } from './context/TaskContext';
 import EditProfileModal from './modals/EditProfileModal';
 import { MdEdit } from 'react-icons/md';
+import AddTaskModal from './task/AddTaskModal';
 
 const override = {
     display: 'block',
